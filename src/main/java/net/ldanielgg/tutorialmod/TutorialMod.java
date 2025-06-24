ab97603592eca96c98e9b134cf571e6716956a47
ldanielgg <ldanielgg@hotmail.com>
2025-06-24 18:46:17 -0400

    added pink garnet blocks

--- src/main/java/net/ldanielgg/tutorialmod/TutorialMod.java
+++ src/main/java/net/ldanielgg/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.ldanielgg.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ldanielgg.tutorialmod.block.ModBlocks;
 import net.ldanielgg.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
