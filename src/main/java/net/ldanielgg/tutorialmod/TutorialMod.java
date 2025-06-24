1e3e1afb6c288a3394036a998a471b69cbd60bf4
ldanielgg <ldanielgg@hotmail.com>
2025-06-24 18:54:04 -0400

    added item groups for pink garnet items

--- src/main/java/net/ldanielgg/tutorialmod/TutorialMod.java
+++ src/main/java/net/ldanielgg/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.ldanielgg.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.ldanielgg.tutorialmod.block.ModBlocks;
+import net.ldanielgg.tutorialmod.item.ModItemGroups;
 import net.ldanielgg.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,5 +16,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+		ModItemGroups.registerItemGroups();
 	}
 }
